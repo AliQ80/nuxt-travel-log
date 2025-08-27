5090ae24327442f93a029553efef562e4c1d3e70
Ali Karam <AK@alikaram.com>
2025-08-27 22:28:07 +0300

    setup tailwind / daisy ui / icons (#49)
    - closes #10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,20 @@
+import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
   eslint: {
     config: {
       standalone: false,
     },
   },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ['../assets/css/main.css'],
 })
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+export default {
+  // v4 usually doesn’t need `content` here when using the Vite plugin
+  theme: {
+    extend: {},
+  },
+  // DaisyUI is already enabled via the CSS @plugin line
+  plugins: [],
+  // Optional DaisyUI config:
+  // daisyui: {
+  //   themes: ['light', 'dark'], // or your preferred list
+  // },
+}
