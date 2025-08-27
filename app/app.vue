06946fd65d9cbc28790bed77b4535829c0bf33e5
Ali Karam <ak@alikaram.com>
2025-08-27 22:23:32 +0300

    setup tailwind / daisy ui / icons

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <h1 class="m-4 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-primary">
+      View Map <Icon name="tabler:map" />
+    </button>
   </div>
 </template>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,39 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
+import eslintParserVue from 'vue-eslint-parser'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   antfu({
     ignores: ['.pnpm-store/**'],
   }),
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: eslintParserVue,
+    },
+  },
+  {
+    plugins: {
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+    },
+    rules: {
+      // enable all recommended rules to report a warning
+      ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
+      // enable all recommended rules to report an error
+      ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+
+      // or configure rules individually
+      'better-tailwindcss/enforce-consistent-line-wrapping': ['warn', { printWidth: 100 }],
+    },
+    settings: {
+      'better-tailwindcss': {
+        // tailwindcss 4: the path to the entry file of the css based tailwind config (eg: `src/global.css`)
+        entryPoint: './assets/css/main.css',
+        // tailwindcss 3: the path to the tailwind config file (eg: `tailwind.config.js`)
+        tailwindConfig: 'tailwind.config.js',
+      },
+    },
+  },
 )
